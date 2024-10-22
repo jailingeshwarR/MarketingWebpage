8fce399079011ccad26bca455ecfecb280105922
JailingeshwarR <jailingeshwar.jr@gmail.com>
2024-10-22 23:28:10 +0530

    add db and prisma changes

---
+++ prisma/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let db: any;
+
+db = new PrismaClient();
+
+const initializePrisma = async () => {
+  db.$queryRaw`PRAGMA busy_timeout = 5000;`;
+};
+initializePrisma()
+  .then(() => {})
+  .catch((error) => console.error(error));
+
+export default db;
