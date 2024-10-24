d646f7f61ffe12e2f5212bec55d5e0520710bee2
JailingeshwarR <jailingeshwar.jr@gmail.com>
2024-10-25 02:07:10 +0530

    add footer and icons

---
+++ src/components/ui/icons/mail.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Mail = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24">
+      <g
+        fill="none"
+        stroke="black"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2">
+        <rect width="20" height="16" x="2" y="4" rx="2" />
+        <path d="m22 7l-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+      </g>
+    </svg>
+  );
+};
+
+export default Mail;
