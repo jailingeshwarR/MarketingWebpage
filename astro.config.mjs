0e5dde2760c831a5b0c0d9e9232bd9169e0fe402
JailingeshwarR <jailingeshwar.jr@gmail.com>
2024-10-25 15:48:35 +0530

    build fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon(),react()],
-  output:"server"
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
