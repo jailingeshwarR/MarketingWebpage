26dcc6247af68cf0376848440393a0bfa2ce46a4
JailingeshwarR <jailingeshwar.jr@gmail.com>
2024-11-11 19:30:17 +0530

    add albums

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,9 +31,20 @@ const teamCollection = defineCollection({
   }),
 });
 
+const albums = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      cover: image(),
+    }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
+  'albums': albums,
   'blog': blogCollection,
-  'team': teamCollection,
+  'team': teamCollection
 };
