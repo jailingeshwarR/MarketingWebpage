b6428b44efead06ea61468e1b7a5acc1bdb94223
JailingeshwarR <jailingeshwar.jr@gmail.com>
2024-11-12 12:29:54 +0530

    fix albums by prerender

--- src/content/config.ts
+++ src/content/config.ts
@@ -44,7 +44,7 @@ const albums = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'albums': albums,
+  albums,
   'blog': blogCollection,
   'team': teamCollection
 };
